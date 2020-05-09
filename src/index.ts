2902813b14d38ab0c9109e292a4b90b0a11639ee
ebenn <eric@bennfamily.com>
2020-05-09 13:10:18 -0400

    WIP Splash Screen

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
-import { Vector3, ActionManager, HemisphericLight } from 'babylonjs';
+import { Vector3, ActionManager, HemisphericLight, DefaultLoadingScreen } from 'babylonjs';
 import { BabylonStore } from './store/babylonStore';
-import { Spawner } from './util/spawner';
+/*import { Spawner } from './util/spawner';
 import { Garden } from './environment/garden';
 import { Farmer } from './player/farmer';
-import { PlayerCameraController } from './camera/playerCameraController';
+import { PlayerCameraController } from './camera/playerCameraController';*/
+
+function loadingScreenCallback(): void {
+    const loadingDiv = document.createElement('div');
+    loadingDiv.id = 'loadingDiv';
+    loadingDiv.style.backgroundColor = 'white';
+    loadingDiv.style.opacity = '1';
+    loadingDiv.style.transition = 'opacity 1.5s ease';
+    loadingDiv.style.pointerEvents = 'none';
+    loadingDiv.style.background = 'url(https://storage.googleapis.com/farmer-assets/splashScreen/1/splash.jpg) no-repeat';
+    loadingDiv.style.width = '100%';
+    loadingDiv.style.height = '100%';
+    loadingDiv.style.backgroundSize = 'cover';
+
+    document.body.appendChild(loadingDiv);
+}
 
 /**
  * The entrypoint for the game.
@@ -24,7 +39,11 @@ export class Game {
         BabylonStore.scene.useRightHandedSystem = true;
 
         new HemisphericLight("light1", new Vector3(0, 1, 0), BabylonStore.scene);
-        Spawner.create('Garden', 'https://storage.googleapis.com/farmer-assets/garden/Environment.gltf').then(() => {
+
+        DefaultLoadingScreen.prototype.displayLoadingUI = loadingScreenCallback;
+        BabylonStore.engine.displayLoadingUI();
+
+        /*Spawner.create('Garden', 'https://storage.googleapis.com/farmer-assets/garden/Environment.gltf').then(() => {
             new Garden();
         });
 
@@ -33,7 +52,7 @@ export class Game {
             const player = new Farmer();
             new PlayerCameraController(player);
             this.#_render = true;
-        });
+        });*/
 
         window.addEventListener('resize', () => {
             if(this.#_render) {
